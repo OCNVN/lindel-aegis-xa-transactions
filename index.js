f67cf7cfd4599add97b570ab58e43bf76b33c196
Carlos Lucero <nandolucero@icloud.com>
2014-10-06 02:51:19 -0500

    Arreglos en el worker

---
+++ index.js
@@ -0,0 +1,5 @@
+var DistributedTransactionClient = require("./lib/distributed-transaction-client").DistributedTransactionClient;
+var	DistributedTransactionWorker = require("./lib/distributed-transaction-worker").DistributedTransactionWorker;
+
+exports.DistributedTransactionClient = DistributedTransactionClient;
+exports.DistributedTransactionWorker = DistributedTransactionWorker;
